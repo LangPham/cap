ebf3f0bf9a63329f7ab42bdd53df170368cf3b29
Lang Pham <phamxuanlang@gmail.com>
2021-06-21 17:43:31 +0700

    Splip apply_abac

--- lib/cap/abac.ex
+++ lib/cap/abac.ex
@@ -3,8 +3,16 @@ defmodule Cap.Abac do
 	def apply_abac(req, resource)do
 		list_check = [:show, :edit, :update, :delete]
 		if req.plug_opts in list_check do
-			module = req.plug
-			has_abac = Keyword.has_key?(module.__info__(:functions), :abac)
+			check_abac(req, resource)
+		else
+			true
+		end
+	end
+	
+	defp check_abac(req, resource)do
+		module = req.plug
+		has_abac = Keyword.has_key?(module.__info__(:functions), :abac)
+		if has_abac do
 			%{"id" => id} = req.path_params
 			check_id = apply(module, :abac, [id])
 			value_to_string(check_id) == resource.id
